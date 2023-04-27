86761e134813491da16750c9c237c99ac83971b5
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-26 22:52:46 -0500

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel(),
 });
