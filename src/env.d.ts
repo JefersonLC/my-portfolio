0809d6c27c10e53e3609f6b13b011eb2893e0908
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-28 15:24:30 -0500

    env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly SECRET: string
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
