f4f4095169cbd8e0c634085627b2a256389b2454
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-08 21:37:57 -0500

    portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: vercel(),
 });
