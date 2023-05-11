8ac86871230f8cb3c624629922d0710eda8c13a7
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-10 19:13:04 -0500

    env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,4 @@
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-    readonly SECRET: string
-    readonly API_URL: string
-  }
   
   interface ImportMeta {
     readonly env: ImportMetaEnv
