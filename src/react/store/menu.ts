aa3f3d1f3f50c59fcd896daac96aa75f832d60f5
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-15 22:28:57 -0500

    zustand store

--- src/react/store/menu.ts
+++ src/react/store/menu.ts
@@ -1,23 +1,19 @@
-// import { create } from 'zustand';
+import { create } from 'zustand';
 
-// interface MenuState {
-//   isVisible: boolean;
-//   showMenu: () => void;
-//   hideMenu: () => void;
-// }
+interface MenuState {
+  isVisible: boolean;
+  showMenu: () => void;
+  hideMenu: () => void;
+}
 
-// export const useMenu = create<MenuState>()((set) => {
-//   const body = document.getElementById('body') as HTMLBodyElement;
-
-//   return {
-//     isVisible: false,
-//     showMenu: () => {
-//       set((state) => ({ isVisible: true }));
-//       body.classList.add('overflow-hidden');
-//     },
-//     hideMenu: () => {
-//       set((state) => ({ isVisible: false }));
-//       body.classList.remove('overflow-hidden');
-//     },
-//   };
-// });
+export const useMenu = create<MenuState>()((set) => {
+  return {
+    isVisible: false,
+    showMenu: () => {
+      set((state) => ({ isVisible: true }));
+    },
+    hideMenu: () => {
+      set((state) => ({ isVisible: false }));
+    },
+  };
+});
