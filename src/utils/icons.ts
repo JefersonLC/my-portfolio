6d29413e06fb607064f2b01615e0e62e90f06d22
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-02 17:16:05 -0500

    icons list

---
+++ src/utils/icons.ts
@@ -0,0 +1,43 @@
+import HtmlIcon from '../icons/HtmlIcon.astro';
+import CssIcon from '../icons/CssIcon.astro';
+import TailWindIcon from '../icons/TailwindIcon.astro';
+import JavaScriptIcon from '../icons/JavaScriptIcon.astro';
+import TypeScriptIcon from '../icons/TypeScriptIcon.astro';
+import ReactIcon from '../icons/ReactIcon.astro';
+import AstroIcon from '../icons/AstroIcon.astro';
+import NodeIcon from '../icons/NodeIcon.astro';
+
+export const Icons = [
+  {
+    name: 'HTML',
+    icon: HtmlIcon,
+  },
+  {
+    name: 'CSS',
+    icon: CssIcon,
+  },
+  {
+    name: 'Tailwind',
+    icon: TailWindIcon,
+  },
+  {
+    name: 'JavaScript',
+    icon: JavaScriptIcon,
+  },
+  {
+    name: 'TypeScript',
+    icon: TypeScriptIcon,
+  },
+  {
+    name: 'React',
+    icon: ReactIcon,
+  },
+  {
+    name: 'Astro',
+    icon: AstroIcon,
+  },
+  {
+    name: 'Node',
+    icon: NodeIcon,
+  },
+];
