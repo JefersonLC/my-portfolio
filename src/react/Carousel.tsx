c80d24ae952605c2bc7b6d4c7ad968c7e49b843e
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-05 21:44:02 -0500

    updated Carousel component

--- src/react/Carousel.tsx
+++ src/react/Carousel.tsx
@@ -1,16 +1,19 @@
 import { Carousel as AntCarousel } from 'antd';
-import { Icons } from '../utils/icons';
+import { icons } from '../utils/icons';
 
 export default function Carousel() {
   return (
-    <AntCarousel autoplay>
-      {Icons.map((section, i) => (
-        <div key={i}>
-          {section.map((Icon, e) => (
-            <div key={e}>
-              <Icon.icon />
-            </div>
-          ))}
+    <AntCarousel autoplay className='pb-20'>
+      {icons.map((section, index) => (
+        <div key={index}>
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-12 mt-6'>
+            {section.map(({ name, Icon }) => (
+              <div key={name} className='flex items-center gap-5'>
+                <Icon />
+                <span className='text-lg text-gray-100'>{name}</span>
+              </div>
+            ))}
+          </div>
         </div>
       ))}
     </AntCarousel>
