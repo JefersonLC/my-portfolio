aa7d9619b7c6d9d7cc23391006e02366ca5054b7
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-29 23:49:03 -0500

    removed

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,47 +1,47 @@
-import HtmlIcon from '../react/icons/HtmlIcon';
-import CssIcon from '../react/icons/CssIcon';
-import TailWindIcon from '../react/icons/TailwindIcon';
-import JavaScriptIcon from '../react/icons/JavaScriptIcon';
-import TypeScriptIcon from '../react/icons/TypeScriptIcon';
-import ReactIcon from '../react/icons/ReactIcon';
-import AstroIcon from '../react/icons/AstroIcon';
-import NodeIcon from '../react/icons/NodeIcon';
+// import HtmlIcon from '../react/icons/HtmlIcon';
+// import CssIcon from '../react/icons/CssIcon';
+// import TailWindIcon from '../react/icons/TailwindIcon';
+// import JavaScriptIcon from '../react/icons/JavaScriptIcon';
+// import TypeScriptIcon from '../react/icons/TypeScriptIcon';
+// import ReactIcon from '../react/icons/ReactIcon';
+// import AstroIcon from '../react/icons/AstroIcon';
+// import NodeIcon from '../react/icons/NodeIcon';
 
-export const icons = [
-  [
-    {
-      name: 'HTML',
-      Icon: HtmlIcon,
-    },
-    {
-      name: 'CSS',
-      Icon: CssIcon,
-    },
-    {
-      name: 'Tailwind',
-      Icon: TailWindIcon,
-    },
-    {
-      name: 'JavaScript',
-      Icon: JavaScriptIcon,
-    },
-  ],
-  [
-    {
-      name: 'TypeScript',
-      Icon: TypeScriptIcon,
-    },
-    {
-      name: 'React',
-      Icon: ReactIcon,
-    },
-    {
-      name: 'Astro',
-      Icon: AstroIcon,
-    },
-    {
-      name: 'Node',
-      Icon: NodeIcon,
-    },
-  ],
-];
+// export const icons = [
+//   [
+//     {
+//       name: 'HTML',
+//       Icon: HtmlIcon,
+//     },
+//     {
+//       name: 'CSS',
+//       Icon: CssIcon,
+//     },
+//     {
+//       name: 'Tailwind',
+//       Icon: TailWindIcon,
+//     },
+//     {
+//       name: 'JavaScript',
+//       Icon: JavaScriptIcon,
+//     },
+//   ],
+//   [
+//     {
+//       name: 'TypeScript',
+//       Icon: TypeScriptIcon,
+//     },
+//     {
+//       name: 'React',
+//       Icon: ReactIcon,
+//     },
+//     {
+//       name: 'Astro',
+//       Icon: AstroIcon,
+//     },
+//     {
+//       name: 'Node',
+//       Icon: NodeIcon,
+//     },
+//   ],
+// ];
