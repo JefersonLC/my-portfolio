d7e7f6d981ab6d3a21477746c57410ae02bac17d
JefersonLC <c.jeferson.lc@gmail.com>
2023-06-30 00:11:00 -0500

    updated utils

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,47 +1,60 @@
-// import HtmlIcon from '../react/icons/HtmlIcon';
-// import CssIcon from '../react/icons/CssIcon';
-// import TailWindIcon from '../react/icons/TailwindIcon';
-// import JavaScriptIcon from '../react/icons/JavaScriptIcon';
-// import TypeScriptIcon from '../react/icons/TypeScriptIcon';
-// import ReactIcon from '../react/icons/ReactIcon';
-// import AstroIcon from '../react/icons/AstroIcon';
-// import NodeIcon from '../react/icons/NodeIcon';
+import CSSIcon from '../icons/skills/CSSIcon.astro'
+import ExpressIcon from '../icons/skills/ExpressIcon.astro'
+import GitIcon from '../icons/skills/GitIcon.astro'
+import HTMLIcon from '../icons/skills/HTMLIcon.astro'
+import JavaScriptIcon from '../icons/skills/JavaScriptIcon.astro'
+import MySQLIcon from '../icons/skills/MySQLIcon.astro'
+import NextIcon from '../icons/skills/NextIcon.astro'
+import ReactIcon from '../icons/skills/ReactIcon.astro'
+import SupabaseIcon from '../icons/skills/SupabaseIcon.astro'
+import TailWindIcon from '../icons/skills/TailwindIcon.astro'
+import TypeScriptIcon from '../icons/skills/TypeScriptIcon.astro'
 
-// export const icons = [
-//   [
-//     {
-//       name: 'HTML',
-//       Icon: HtmlIcon,
-//     },
-//     {
-//       name: 'CSS',
-//       Icon: CssIcon,
-//     },
-//     {
-//       name: 'Tailwind',
-//       Icon: TailWindIcon,
-//     },
-//     {
-//       name: 'JavaScript',
-//       Icon: JavaScriptIcon,
-//     },
-//   ],
-//   [
-//     {
-//       name: 'TypeScript',
-//       Icon: TypeScriptIcon,
-//     },
-//     {
-//       name: 'React',
-//       Icon: ReactIcon,
-//     },
-//     {
-//       name: 'Astro',
-//       Icon: AstroIcon,
-//     },
-//     {
-//       name: 'Node',
-//       Icon: NodeIcon,
-//     },
-//   ],
-// ];
+//Astro, Node
+
+export const icons = [
+  {
+    name: 'HTML',
+    Icon: HTMLIcon,
+  },
+  {
+    name: 'CSS',
+    Icon: CSSIcon,
+  },
+  {
+    name: 'Tailwind',
+    Icon: TailWindIcon,
+  },
+  {
+    name: 'JavaScript',
+    Icon: JavaScriptIcon,
+  },
+  {
+    name: 'TypeScript',
+    Icon: TypeScriptIcon,
+  },
+  {
+    name: 'React',
+    Icon: ReactIcon,
+  },
+  {
+    name: 'NextJS',
+    Icon: NextIcon,
+  },
+  {
+    name: 'Supabase',
+    Icon: SupabaseIcon,
+  },
+  {
+    name: 'MySQL',
+    Icon: MySQLIcon,
+  },
+  {
+    name: 'Express',
+    Icon: ExpressIcon,
+  },
+  {
+    name: 'Git',
+    Icon: GitIcon,
+  },
+]
