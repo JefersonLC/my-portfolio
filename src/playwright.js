102627c152ca6486b9b039ba6486f563385ce981
JefersonLC <c.jeferson.lc@gmail.com>
2023-07-01 19:56:08 -0500

    updated scrapper

--- src/playwright.js
+++ src/playwright.js
@@ -1,86 +1,102 @@
-import fs from 'fs/promises';
-import { firefox } from 'playwright';
+import fs from 'fs/promises'
+import { firefox } from 'playwright'
 
 const links = [
   'https://github.com/JefersonLC/react_tic-tac-toe',
   'https://github.com/JefersonLC/react_calculator',
-  'https://github.com/JefersonLC/url-shortener'
-];
+  'https://github.com/JefersonLC/url-shortener',
+]
 
-const browser = await firefox.launch({ headless: true });
+const browser = await firefox.launch({ headless: true })
 
-const page = await browser.newPage();
+const page = await browser.newPage()
 
-const PROJECTS = [];
+const PROJECTS = []
 
 // Obteniendo la data
 for (let link of links) {
-  await page.goto(link);
+  await page.goto(link)
 
   const project = await page.$eval('body', (body) => {
-    const title = body.querySelector('readme-toc h1').innerText.trim();
+    const title = body.querySelector('readme-toc h1').innerText.trim()
 
     const description = body
       .querySelector('#responsive-meta-container div .f4')
-      .innerText.trim();
+      .innerText.trim()
 
     const slug = body
       .querySelector('#repository-container-header strong a')
-      .innerText.trim();
+      .innerText.trim()
 
     const languages = new Array(
       ...body.querySelectorAll('.BorderGrid-cell .list-style-none .d-inline')
-    ).map((item) => item.querySelector('a .text-bold').innerText.trim());
+    ).map((item) => item.querySelector('a .text-bold').innerText.trim())
 
     const stars = Number(
       body
         .querySelectorAll('#responsive-meta-container > div > .mb-3 a')[0]
         .innerText.trim()
         .slice(0, 1)
-    );
+    )
 
     const forks = Number(
       body
         .querySelectorAll('#responsive-meta-container > div > .mb-3 a')[1]
         .innerText.trim()
         .slice(0, 1)
-    );
+    )
 
-    const link = body
-      .querySelector('#responsive-meta-container div span')
-      .innerText.trim();
+    let link = ''
 
-    return { title, description, slug, languages, stars, forks, link };
-  });
+    try {
+      const response = body
+        .querySelector('#responsive-meta-container div span a')
+        .innerText.trim()
 
-  const sourceCode = page.url();
+      response && (link = response)
+    } catch (error) {
+      console.log('error')
+    }
 
-  PROJECTS.push({ ...project, sourceCode });
+    return {
+      title,
+      description,
+      slug,
+      languages,
+      stars,
+      forks,
+      link,
+    }
+  })
+
+  const sourceCode = page.url()
+
+  PROJECTS.push({ ...project, sourceCode })
 }
 
-await browser.close();
+await browser.close()
 
 // Creando el JSON
-const jsonData = JSON.stringify(PROJECTS);
+const jsonData = JSON.stringify(PROJECTS)
 
-const path = './src/utils/projects.json';
+const path = './src/utils/projects.json'
 
 fs.access(path)
   .then(() => {
-    console.log('El archivo ya existe. Eliminando el archivo...');
-    return fs.unlink(path);
+    console.log('El archivo ya existe. Eliminando el archivo...')
+    return fs.unlink(path)
   })
   .catch(() => {
     console.log(
       'El archivo no existe o no se puede acceder a él. Realizando alguna acción...'
-    );
+    )
   })
   .finally(() => {
-    return fs.writeFile(path, jsonData);
+    return fs.writeFile(path, jsonData)
   })
   .then(() => {
-    console.log('Archivo JSON creado correctamente.');
+    console.log('Archivo JSON creado correctamente.')
   })
   .catch((err) => {
-    console.error('Error al escribir o eliminar el archivo:', err);
-  });
+    console.error('Error al escribir o eliminar el archivo:', err)
+  })
