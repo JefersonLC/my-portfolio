6333c027d37666e8fca513e392187f72871f205e
JefersonLC <c.jeferson.lc@gmail.com>
2023-08-15 00:40:58 -0500

    add link list

---
+++ src/utils/links.ts
@@ -0,0 +1,14 @@
+export const links = [
+  {
+    href: '#about',
+    children: 'Acerca de mí',
+  },
+  {
+    href: '#projects',
+    children: 'Proyectos',
+  },
+  {
+    href: '#skills',
+    children: 'Habilidades',
+  },
+]
