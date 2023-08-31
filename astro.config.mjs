a01b9986da2625730f71613f0d3e1c6285ffa21e
JefersonLC <c.jeferson.lc@gmail.com>
2023-08-31 18:08:18 -0500

    remove: experimental viewTransitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  experimental: {
-    viewTransitions: true,
-  },
   compressHTML: true,
   site: 'https://jeferson-portfolio.vercel.app/',
 })
