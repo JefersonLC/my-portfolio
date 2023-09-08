c54cde03cd6ce9a950cb17bf1d20756c4e590929
JefersonLC <c.jeferson.lc@gmail.com>
2023-09-08 12:37:53 -0500

    update: import routes

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,13 +1,13 @@
-import CSSIcon from '../icons/skills/CSSIcon.astro'
-import ExpressIcon from '../icons/skills/ExpressIcon.astro'
-import GitIcon from '../icons/skills/GitIcon.astro'
-import HTMLIcon from '../icons/skills/HTMLIcon.astro'
-import JavaScriptIcon from '../icons/skills/JavaScriptIcon.astro'
-import MySQLIcon from '../icons/skills/MySQLIcon.astro'
-import ReactIcon from '../icons/skills/ReactIcon.astro'
-import SupabaseIcon from '../icons/skills/SupabaseIcon.astro'
-import TailWindIcon from '../icons/skills/TailwindIcon.astro'
-import TypeScriptIcon from '../icons/skills/TypeScriptIcon.astro'
+import CSSIcon from '@icons/skills/CSSIcon.astro'
+import ExpressIcon from '@icons/skills/ExpressIcon.astro'
+import GitIcon from '@icons/skills/GitIcon.astro'
+import HTMLIcon from '@icons/skills/HTMLIcon.astro'
+import JavaScriptIcon from '@icons/skills/JavaScriptIcon.astro'
+import MySQLIcon from '@icons/skills/MySQLIcon.astro'
+import ReactIcon from '@icons/skills/ReactIcon.astro'
+import SupabaseIcon from '@icons/skills/SupabaseIcon.astro'
+import TailWindIcon from '@icons/skills/TailwindIcon.astro'
+import TypeScriptIcon from '@icons/skills/TypeScriptIcon.astro'
 
 //Astro, Node
 
