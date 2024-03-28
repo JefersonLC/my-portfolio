d335a963d5c2292a71775b52ce718ef03db40f73
JefersonLC <c.jeferson.lc@gmail.com>
2024-03-28 16:09:51 -0500

    my-portfolio

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    container: { center: true },
-    colors: {
-      blue: {
-        100: '#112240',
-        200: '#0a192f',
-      },
-      green: '#64ffda',
-      white: '#ffffff',
-      gray: {
-        100: '#ccd6f6',
-        200: '#8892b0',
-      },
-    },
-  },
-  plugins: [],
-  darkMode: 'class',
-};
+  plugins: []
+}
